804ca138b3ad084e78503fe22d4c783b2f6d9df9
Hackbright Student <info@hackbrightacademy.com>
2015-02-12 17:28:19 -0800

    Added pearson correlation

--- model.py
+++ model.py
@@ -22,6 +22,49 @@ class User(Base):
     age = Column(Integer, nullable=True)
     zipcode = Column(String(15), nullable=True)
 
+    def similarity(self, other):
+        u_ratings = {}
+        paired_ratings = []
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.rating, r.rating) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+        else:
+            return 0.0
+
+    def predict_rating(self, movie):
+        ratings = self.ratings
+        other_ratings = movie.ratings
+        similarities = [ (self.similarity(r.user), r) \
+            for r in other_ratings ]
+        similarities.sort(reverse = True)
+        similarities = [ sim for sim in similarities if sim[0] > 0 ]
+        if not similarities:
+            return None
+        numerator = sum([ r.rating * similarity for similarity, r in similarities ])
+        denominator = sum([ similarity[0] for similarity in similarities ])
+        return numerator/denominator
+    
+    # def predict_rating(self, movie):
+    #     ratings = self.ratings
+    #     other_ratings = movie.ratings
+    #     other_users = [ r.user for r in other_ratings ]
+    #     similarities = [ (self.similarity(other_user), other_user) \
+    #         for other_user in other_users ]
+    #     similarities.sort(reverse = True)
+    #     top_user = similarities[0]
+    #     matched_rating = None
+    #     for rating in other_ratings:
+    #         if rating.user_id == top_user[1].id:
+    #             matched_rating = rating
+    #             break
+    #     return matched_rating.rating * top_user[0]
 
     def __repr__(self):
         return "User id = %d, email = %s, password = %s!" % (
@@ -53,9 +96,9 @@ class Rating(Base):
     user = relationship("User",
             backref=backref("ratings", order_by=id))
 
-    # def __repr__(self):
-    #     return "Rating id = %d, movie id = %d, user id = %d, rating = %d" % (
-    #     self.id, self.movie_id, self.user_id, self.rating)
+    def __repr__(self):
+        return "Rating id = %d, movie id = %d, user id = %d, rating = %d" % (
+        self.id, self.movie_id, self.user_id, self.rating)
 
 def get_user_by_email(email_address):
     """returns a user by email address from database"""
